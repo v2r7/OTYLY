dac320fc49a1e60aef79049832a4f4ae5bcabe1f
Lors ❄️ <165544864+v2r7@users.noreply.github.com>
2024-04-16 09:42:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,167 +1,182 @@
-const Discord = require("discord.js");
-const moment = require('moment')
+///coded by : 
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("Tax bot By Ra3d"));
+
+///coded by : 
+const Discord = require('discord.js');
 const client = new Discord.Client();
-require('http').createServer((req, res) => res.end('bot  is alive')).listen(3000)
-client.on('ready', async () => {
-  console.log(`${client.user.tag}`)
-})
-
-
-setTimeout(() => {
-  if (!client || !client.user) {
-    console.log("Client Not Login, Process Kill")
-    process.kill(1);
-  } else {
-    console.log("Client Login")
-  }
-}, 3 * 1000 * 60);
-client.login(process.env.token);
-
-///////
-client.on("message", message => {
-  if (message.content.startsWith('$')) {
-
-    let embed100 = new Discord.MessageEmbed()
- .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For Old Host`)
-.setDescription(`## طرق الدفع 
-
-\`\`\`Stc Pay
-
-AL-Rajhi Bank
-
-Razer Gold
-
-iTunes\`\`\` `)
-      .setColor('#43b581')
-    message.channel.send('||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed100});
-      }
-    });
-
-///////////////////
-
-client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag} Online`);
-client.user.setActivity('- Old Host .', { type: 'PLAYING' })
-client.user.setStatus("dnd");
+///coded by : 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(">| Stats :-");
+  console.log(`Servers : > " ${client.guilds.cache.size} " `);
+  console.log(`Users : > " ${client.users.cache.size} " `);
+  console.log(`channels : > " ${client.channels.cache.size} " `);
+  console.log(">| Date :");
+  console.log(`> ${new Date()}`);
+  console.log(">| Name :");
+  console.log(`> ${client.user.username}`);
+  client.user.setActivity('#tax || #wa ', ({type: "WATCHING"})) //حط قيم الي تبيها
 });
+ 
+ 
+client.on("message", async message => {
+///coded by : 
 
-/////////
+   
+const prefix = "+";//برفكس حقك
+const devs = "762429683483672647";//اي دي حقك
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" "); if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+///coded by : Ra
+  if (message.content.toLowerCase().startsWith(prefix + "+help".toLowerCase())) { 
+    let help = new Discord.MessageEmbed()
+      .setColor("0x5016f3")
+      .setDescription(`**( Probot Taxs . :rocket: )
+      
+        --------------------------------------------------
+                
+               Probot Commands :
+                
+               1-  ${prefix}tax 
+معلومة اذا تبي تكتب الضريبه اكتب 500000 لاتكتب 500k !!!
+        --------------------------------------------------
+        Developers : <@1114863041058713680>
+            **`);
+    message.channel.send(help);
+  }
+///coded 
+  if (message.content.toLowerCase().startsWith(prefix + "".toLowerCase())) { 
+    let args2 = parseInt(args)
+    let tax = Math.floor(args2 * (20) / (19) + (1))
+    let tax2 = Math.floor(args2 * (20) / (19) + (1)-(args2))
+    let tax3 = Math.floor(tax2 * (20) / (19) + (1))
+    let tax4 = Math.floor(tax2 + tax3 + args2)
+    let errorembed3 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**It Must be a number**`)
+    .setFooter(`${client.user.username}`);
+    if (!args2) return message.channel.send(errorembed3);
+    let errorembed2 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must Be A Number**`)
+    .setFooter(`${client.user.username}`);
+    if (isNaN(args2)) return message.channel.send(errorembed2);
+    let errorembed = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must The Number Larger 1**`)
+    .setFooter(`${client.user.username}`);
+    if (args2 < 1) return message.channel.send(errorembed);
+    let embed3 = new Discord.MessageEmbed()
+    .setTitle(`**:notes: The Final Cost Is :**`)
+    .setColor("#43b581")
+    .setDescription(`1`)
+    .setFooter(`${client.user.username}`);
+    if (args2 == 1) return message.channel.send(embed3);
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`**:notes: The Final Cost Is :**`)
+    .setColor("#43b581")
+    .setDescription(`**ضريبه الوسيط** : **${tax2}**
+                      **ضريبة التحويل** : **${tax3}**
+                      **المبلغ المطلوب تحويله** : **${tax4}**`)
+    .setFooter(`${client.user.username}`);
+    message.channel.send(embed);
+  }
 
-client.on("message", message => {
-  if (message.content.startsWith('AL-Rajhi Bank')) {
 
-    let embed100 = new Discord.MessageEmbed()
+
+///coded by : 
+  if (message.content.toLowerCase().startsWith("tax".toLowerCase())) { 
+    let args2 = parseInt(args)
+    let tax = Math.floor(args2 * (20) / (19) + (1))
+    let errorembed3 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**It Must be a number**`)
+    .setFooter(`${client.user.username}`);
+    if (!args2) return message.channel.send(errorembed3);
+    let errorembed2 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must Be A Number**`)
+    .setFooter(`${client.user.username}`);
+    if (isNaN(args2)) return message.channel.send(errorembed2);
+    let errorembed = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must The Number Larger 1**`)
+    .setFooter(`${client.user.username}`);
+    if (args2 < 1) return message.channel.send(errorembed);
+    let embed3 = new Discord.MessageEmbed()
+    .setTitle(`**:notes: The Final Cost Is :**`)
+    .setColor("#43b581")
+    .setDescription(`1`)
+    .setFooter(`${client.user.username}`);
+    if (args2 == 1) return message.channel.send(embed3);
+    let embed = new Discord.MessageEmbed()
        .setFooter(`Developer Development For Old Host`)
       .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For OldLaw`)
-.setDescription(`## AL-Rajhi Bank
+    .setColor("#43b581")
+    .setTitle(`**ضريبة المبلغ : ${tax}
+    يرجى التحويل في روم <#>**`);
+    message.channel.send('||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed});
+  }
 
+  
 
-\`\`\`رقم : 478608010046830
+///coded by : Ra3d🍁#9999 - AbdulElah#0437 - Ln,Muziky#8956
+ 
+  
+let ac = ["listening", "watching", "playing", "streaming","default"];
+let ac2 = ["online", "idle", "invisible", "dnd","default"];
 
-الايبان : 80000 4786080 1004 6830
+ let args0 = message.content.split(" ");
+  if (args0[0] === (prefix + 'settings')) {
+   if (!devs.includes(message.author.id)) return;
+    if (args0[1] === 'setname') {
+      if (!args0[2]) return message.channel.send('**:x: Please input a new name.**');
+      await client.user.setUsername(args0.slice(2).join(" "));
+ let changedname = new Discord.MessageEmbed()
+  .setTimestamp().setColor("GREEN").setAuthor(message.author.username, message.author.displayAvatarURL()).setFooter(client.user.username,client.user.displayAvatarURL()).setThumbnail(message.guild.iconURL())
+  .setDescription(`**✅ Done , The bot name changed to : ${args0.slice(2).join(" ")}**`)
+  message.channel.send(changedname)
+       message.react('✅');
+    } else if (args0[1] === 'setavatar') {
+      if (!args0[2]) return message.channel.send('**:x: Please input an avatar URL.**');
+      await client.user.setAvatar(args0.slice(2).join(" "));
+ let changedavatar = new Discord.MessageEmbed()
+  .setTimestamp().setImage(args0.slice(2).join(" ")).setColor("GREEN").setAuthor(message.author.username, message.author.displayAvatarURL()).setFooter(client.user.username,client.user.displayAvatarURL()).setThumbnail(message.guild.iconURL())
+  .setDescription(`**✅ The bot avatar was changed to :**`)
+  message.channel.send(changedavatar)
+       message.react('✅');
+    } else { 
+       message.channel.send(`> **:bulb: Usage**: ${prefix}settings [ \`setname\` - \`setavatar\` - ]`);
+       message.react('ℹ️');
+    }
+  }
 
-الغرض : حوالات شخصيه 
-
-SA 2.80
-\`\`\`
-### يرجى ارسال صورة الإيصال !!!`)
-   .setColor('#43b581')
-    message.channel.send('\`\`\` رقم : 478608010046830                                                                                             الايبان : 80000 4786080 6830\`\`\`  ||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed100});
+///coded by : 6  
+  
+      if (message.content === prefix + "ping") {
+        const betha = new Discord.MessageEmbed()
+        .setTitle('Your PING')
+        .setColor("RANDOM")
+        .addField([`${Date.now() - message.createdTimestamp}` + "MS"])
+        .setAuthor(message.author.username, message.author.avatarURL({ format: 'png', dynamic: true }))
+      message.channel.send(betha)
       }
-    });
-
-//////////
-
-client.on("message", message => {
-  if (message.content.startsWith('Stc Pay')) {
-
-    let embed10 = new Discord.MessageEmbed()
- .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For Old Host`)
-.setDescription(`## Stc Pay
+});
 
 
-\`\`\`‏Stc Pay : 0508776609
+///coded by :
 
-الغرض من الحوالة : مصاريف للأهل والأصدقاء
-\`\`\`
-### يرجى ارسال صورة الإيصال !!!`)
-   .setColor('#43b581')
-    message.channel.send('\`\`\`Stc Pay : 0508776609\`\`\`  ||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed10});
-      }
-    });
-
-/////////////////
-
-client.on("message", message => {
-  if (message.content.startsWith('Razer Gold')) {
-
-    let embed1 = new Discord.MessageEmbed()
- .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For Old Host`)
-.setDescription(`## Razer Gold
-
-## نقبل :
-\`\`\`
-ريزر قولد عالمي
-
-ريزر قولد امريكي
-\`\`\`
-### يرجى ارسال صورة البطاقة مع الايصال !!!`)
-   .setColor('#43b581')
-    message.channel.send('  ||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed1});
-      }
-    });
-
-//////////////
-
-client.on("message", message => {
-  if (message.content.startsWith('iTunes')) {
-
-    let embed111 = new Discord.MessageEmbed()
- .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For Old Host`)
-.setDescription(`## iTunes
-
-## نقبل :
-\`\`\`
-ايتونز امريكي فقط
-\`\`\`
-### يرجى ارسال صورة البطاقة !!!`)
-   .setColor('#43b581')
-    message.channel.send('  ||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed111});
-      }
-    });
-
-///////////
-
-client.on("message", message=>{
-if(message.content.startsWith("خط")){
-message.channel.send(`اصبر ي قلبي خل يجي البكج 🤦`)
-message.delete()
-}
-})
-
-/////////////////
-
-client.on("message", message => {
-  if (message.content.startsWith('-')) {
-
-    let embed100 = new Discord.MessageEmbed()
- .setThumbnail(`https://cdn.discordapp.com/icons/870478765455732736/a_16d3dc47d2844892f4097b6e8b17112c.gif?size=1024`)
-         .setFooter(`Developer Development For Old Host`)
-.setDescription(`## طرق الدفع 
-
-\`\`\`Stc Pay
-
-AL-Rajhi Bank
-
-Razer Gold
-
-iTunes\`\`\` `)
-      .setColor('#43b581')
-    message.channel.send('||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _ @here', {embed: embed100});
-      }
-    });
+client.login(process.env.token); //توكن بوتك هنا
